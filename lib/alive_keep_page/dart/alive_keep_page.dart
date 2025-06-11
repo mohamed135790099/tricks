530022100555467e17a2d9e6a8b74cd991bc82a0
mohamed hussien <mh1749555@gmail.com>
2025-06-11 22:58:12 +0300

    edite alive keep

--- lib/alive_keep_page/dart/alive_keep_page.dart
+++ lib/alive_keep_page/dart/alive_keep_page.dart
@@ -1,9 +1,16 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 class AliveKeepPage extends StatelessWidget {
   const AliveKeepPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  const SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Text('Alive Keep Page Example'),
+        )
+      ),
+    );
   }
 }
