d909ad9bb4ea0ba1b0549e2e82459c3de8c41fc8
mohamed hussien <mh1749555@gmail.com>
2025-06-17 01:38:30 +0300

    Add New Feature Logic Authentication

---
+++ lib/logic_authentication/logic_authentication.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LogicAuthentication extends StatefulWidget {
+  const LogicAuthentication({super.key});
+
+  @override
+  State<LogicAuthentication> createState() => _LogicAuthenticationState();
+}
+
+class _LogicAuthenticationState extends State<LogicAuthentication> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
