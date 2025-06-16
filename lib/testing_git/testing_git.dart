702f4a7f3a5d85981f77f3cbd09f42d7fab591df
mohamed hussien <mh1749555@gmail.com>
2025-06-16 23:48:39 +0300

    Add New Page testing_git.dart                                                                -on this page we will explain all git command for students

---
+++ lib/testing_git/testing_git.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TestingGit extends StatefulWidget {
+  const TestingGit({super.key});
+
+  @override
+  State<TestingGit> createState() => _TestingGitState();
+}
+
+class _TestingGitState extends State<TestingGit> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
