57e57fee0df86148251967c13061e635d22337e4
mohamed hussien <mh1749555@gmail.com>
2025-06-17 04:12:24 +0300

    Add signup_authentication.dart
    From this signup_authentication.dart Can Add More Settings About Authentication Can known Vaildations fields correct or not

---
+++ lib/signup_authentication/signup_authentication.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SignUpAuthentication extends StatefulWidget {
+  const SignUpAuthentication({super.key});
+
+  @override
+  State<SignUpAuthentication> createState() => _SignUpAuthenticationState();
+}
+
+class _SignUpAuthenticationState extends State<SignUpAuthentication> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
